336220a2959c2674aefb29cdc59290ae2d91cc8b
Tharcyzo <43484498+Tharcyzo@users.noreply.github.com>
2026-05-07 11:29:39 -0300

    Update article.blade.php
    Changelog:
    
    Improved avatar position
    Fixed username position
    Fixed motto position
    Fixed user status position

--- resources/themes/atom/views/community/article.blade.php
+++ resources/themes/atom/views/community/article.blade.php
@@ -13,24 +13,26 @@
                 style="background: rgba(0, 0, 0, 0.5) url({{ asset(sprintf('assets/images/%s', $article->user ? $article->user->permission->staff_background ?? 'staff-bg.png'  : 'staff-bg.png')) }});">
             </div>
 
-            <a href="{{ route('profile.show', $article->user ?? \App\Models\User::first()) }}" class="absolute top-4 left-1 drop-shadow">
+            <a href="{{ route('profile.show', $article->user ?? \App\Models\User::first()) }}" class="absolute top-0 left-1 drop-shadow">
                 <img style="image-rendering: pixelated;" class="transition duration-300 ease-in-out hover:scale-105"
                     src="{{ setting('avatar_imager') }}{{ $article->user?->look }}&direction=2&head_direction=3&gesture=sml&action=wav"
                     alt="">
             </a>
 
-            <p class="text-2xl font-semibold ml-[70px] text-white -mt-[35px]">
+            <p class="text-2xl font-semibold ml-[70px] text-white -mt-[35px]" style="padding-left: 70px; margin-top: -39px;">
                 {{ $article->user->username ?? setting('hotel_name') }}
             </p>
 
-            <div class="flex w-full items-center justify-between px-4">
+            <div class="flex w-full items-center justify-between px-4" style="padding-left: 70px; margin-top: 13px;">
                 <p class="ml-[57px] text-sm mt-[10px] font-semibold text-gray-500">
                     {{ $article->user->motto ?? setting('start_motto') }}
                 </p>
 
                 @if($article->user)
-                    <div class="w-4 h-4 rounded-full mt-2 {{ $article->user->online ? 'bg-green-600' : 'bg-red-600' }}">
-                    </div>
+                    <div style="margin-top: -10px; margin-right: -7px;">
+				<div class="w-4 h-4 rounded-full mt-2 {{ $article->user->online ? 'bg-green-600' : 'bg-red-600' }}">
+                </div>
+					</div>
                 @endif
             </div>
         </div>
